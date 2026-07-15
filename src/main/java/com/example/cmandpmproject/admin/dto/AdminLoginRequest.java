11ef114a913c36f3fe69fd8236859538f6c25978
lowtempice9489 <b7749abc92@gmail.com>
2026-07-15 15:25:27 +0900

    관리자인증 담당 작성 준비 두번째

--- src/main/java/com/example/cmandpmproject/admin/dto/AdminLoginRequest.java
+++ src/main/java/com/example/cmandpmproject/admin/dto/AdminLoginRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class LoginRequest {
+public class AdminLoginRequest {
 
     @Email
     private String email;
