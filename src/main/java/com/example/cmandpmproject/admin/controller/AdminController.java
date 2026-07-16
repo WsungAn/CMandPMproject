04ba97ad566de11d6d388583f096aa3929dc8d0b
lowtempice9489 <b7749abc92@gmail.com>
2026-07-16 18:03:35 +0900

    [관리자인증 담당 작성]{AdminStatus(Enum)} 연결준비 완료, 변동사항 : 컴파일 에러 제거 후 커밋 및 푸시

--- src/main/java/com/example/cmandpmproject/admin/controller/AdminController.java
+++ src/main/java/com/example/cmandpmproject/admin/controller/AdminController.java
@@ -13,59 +13,59 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class AdminController {
-    private final AdminService adminService;
-
-    // 관리자 리스트 조회
-    @GetMapping
-    public ResponseEntity<List<AdminResponse>> listAdmins(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int limit) {
-        List<AdminResponse> responseList = adminService.listAdmins(page, limit);
-        return ResponseEntity.ok(responseList);
-    }
-
-    // 관리자 리스트 상세 조회
-    @GetMapping("/{id}")
-    public ResponseEntity<AdminResponse> getAdminDetail(@PathVariable Long id) {
-        AdminResponse adminResponse = adminService.getAdminDetail(id);
-        return ResponseEntity.ok(adminResponse);
-    }
-
-    // 관리자 정보 수정
-    @PutMapping("/{id}")
-    public ResponseEntity<AdminResponse> updateInfo(@PathVariable Long id, @RequestBody UpdateAdmin updateAdmin) {
-        AdminResponse adminResponse = adminService.updateInfo(id, updateAdmin);
-        return ResponseEntity.ok(adminResponse);
-    }
-
-    // 관리자 역할 변경
-    @PatchMapping("/{id}/role")
-    public ResponseEntity<AdminResponse> changeRole(@PathVariable Long id, @RequestParam String newRole) {
-        AdminResponse adminResponse = adminService.changeRole(id, newRole);
-        return ResponseEntity.ok(adminResponse);
-    }
-
-
-
-
-    // 내 프로필 수정
-    @PutMapping("/me")
-    public ResponseEntity<AdminResponse> updateMyprofile(@RequestBody UpdateAdminRequest request) {
-        AdminResponse adminResponse = adminService.updateMyprofile(
-                request.getUserId(),
-                new UpdateAdmin(request.getName(), request.getEmail(), request.getPhonenumber())
-        );
-        return ResponseEntity.ok(adminResponse);
-    }
-
-    // 비밀번호 변경
-    @PutMapping("/auth/password")
-    public ResponseEntity<AdminResponse> changePassword(@RequestBody ChangePasswordRequest request) {
-        AdminResponse response = adminService.changePassword(
-                request.getUserId(),
-                request.getOldPassword(),
-                request.getNewPassword()
-        );
-        return ResponseEntity.ok(response);
-    }
+//    private final AdminService adminService;
+//
+//    // 관리자 리스트 조회
+//    @GetMapping
+//    public ResponseEntity<List<AdminResponse>> listAdmins(
+//            @RequestParam(defaultValue = "1") int page,
+//            @RequestParam(defaultValue = "10") int limit) {
+//        List<AdminResponse> responseList = adminService.listAdmins(page, limit);
+//        return ResponseEntity.ok(responseList);
+//    }
+//
+//    // 관리자 리스트 상세 조회
+//    @GetMapping("/{id}")
+//    public ResponseEntity<AdminResponse> getAdminDetail(@PathVariable Long id) {
+//        AdminResponse adminResponse = adminService.getAdminDetail(id);
+//        return ResponseEntity.ok(adminResponse);
+//    }
+//
+//    // 관리자 정보 수정
+//    @PutMapping("/{id}")
+//    public ResponseEntity<AdminResponse> updateInfo(@PathVariable Long id, @RequestBody UpdateAdmin updateAdmin) {
+//        AdminResponse adminResponse = adminService.updateInfo(id, updateAdmin);
+//        return ResponseEntity.ok(adminResponse);
+//    }
+//
+//    // 관리자 역할 변경
+//    @PatchMapping("/{id}/role")
+//    public ResponseEntity<AdminResponse> changeRole(@PathVariable Long id, @RequestParam String newRole) {
+//        AdminResponse adminResponse = adminService.changeRole(id, newRole);
+//        return ResponseEntity.ok(adminResponse);
+//    }
+//
+//
+//
+//
+//    // 내 프로필 수정
+//    @PutMapping("/me")
+//    public ResponseEntity<AdminResponse> updateMyprofile(@RequestBody UpdateAdminRequest request) {
+//        AdminResponse adminResponse = adminService.updateMyprofile(
+//                request.getUserId(),
+//                new UpdateAdmin(request.getName(), request.getEmail(), request.getPhonenumber())
+//        );
+//        return ResponseEntity.ok(adminResponse);
+//    }
+//
+//    // 비밀번호 변경
+//    @PutMapping("/auth/password")
+//    public ResponseEntity<AdminResponse> changePassword(@RequestBody ChangePasswordRequest request) {
+//        AdminResponse response = adminService.changePassword(
+//                request.getUserId(),
+//                request.getOldPassword(),
+//                request.getNewPassword()
+//        );
+//        return ResponseEntity.ok(response);
+//    }
 }
--- src/main/java/com/example/cmandpmproject/admin/service/AdminService.java
+++ src/main/java/com/example/cmandpmproject/admin/service/AdminService.java
@@ -13,87 +13,87 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 @RequiredArgsConstructor
 public class AdminService {
-    private final AdminRepository adminRepository;
-    private final PasswordEncoder passwordEncoder;
-
-// 관리자 리스트 조회
-    @Transactional(readOnly = true)
-    public List<AdminResponse> listAdmins(int page,int limit) {
-        List<Admin> admins = adminRepository.findAll();
-
-        // 페이징
-        int start = (page - 1 ) * limit;
-        int end = Math.min(start + limit, admins.size());
-
-        List<AdminResponse> responses = new ArrayList<>();
-        for (int i = start; i < end; i++) {
-            responses.add(toResponse(admins.get(i)));
-        }
-        return responses;
-    }
-
-// 관리자 리스트 상세 조회
-    @Transactional(readOnly = true)
-    public AdminResponse getAdminDetail(Long id) {
-        Admin admin = getOrThrow(id);
-        return toResponse(admin);
-    }
-
-
-    //관리자 정보 수정
-    @Transactional
-    public AdminResponse updateInfo(Long id, UpdateAdmin updateAdmin) {
-        Admin admin = getOrThrow(id);
-
-        admin.updateInfo(
-                updateAdmin.getName(),
-                updateAdmin.getEmail(),
-                updateAdmin.getPhonenumber()
-        );
-        adminRepository.save(admin);
-        return toResponse(admin);
-    }
-
-    //관리자 역할 변경
-    @Transactional
-    public AdminResponse changeRole(Long id, String newRole) {
-        Admin admin = getOrThrow(id);
-        admin.changeRole(newRole);
-        adminRepository.save(admin);
-        return toResponse(admin);
-    }
-    //관리자 상태 변경
-    @Transactional
-    public SignupResponse signup(SignupRequest request){
-
-        // 이메일 중복 확인
-        if(adminRepository.findByEmail(request.getEmail()).isPresent()){
-            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
-        }
-
-        admin.changePassword(newPassword);
-        adminRepository.save(admin);
-        return toResponse(admin);
-    }
-    private AdminResponse toResponse(Admin admin){
-        return new AdminResponse(
-                admin.getId(),
-                admin.getName(),
-                admin.getEmail(),
-                admin.getPhonenumber(),
-                admin.getRole(),
-                admin.getStatus(),
-                admin.getCreatedAt(),
-                admin.getApprovedAt(),
-                admin.getRejectedAt(),
-                admin.getRejectionReason()
-        );
-    }
-
-    // 내부 공통 매서드
-    private Admin getOrThrow(Long id) {
-        return adminRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(
-                        HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다." + id));
-    }
+//    private final AdminRepository adminRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//// 관리자 리스트 조회
+//    @Transactional(readOnly = true)
+//    public List<AdminResponse> listAdmins(int page,int limit) {
+//        List<Admin> admins = adminRepository.findAll();
+//
+//        // 페이징
+//        int start = (page - 1 ) * limit;
+//        int end = Math.min(start + limit, admins.size());
+//
+//        List<AdminResponse> responses = new ArrayList<>();
+//        for (int i = start; i < end; i++) {
+//            responses.add(toResponse(admins.get(i)));
+//        }
+//        return responses;
+//    }
+//
+//// 관리자 리스트 상세 조회
+//    @Transactional(readOnly = true)
+//    public AdminResponse getAdminDetail(Long id) {
+//        Admin admin = getOrThrow(id);
+//        return toResponse(admin);
+//    }
+//
+//
+//    //관리자 정보 수정
+//    @Transactional
+//    public AdminResponse updateInfo(Long id, UpdateAdmin updateAdmin) {
+//        Admin admin = getOrThrow(id);
+//
+//        admin.updateInfo(
+//                updateAdmin.getName(),
+//                updateAdmin.getEmail(),
+//                updateAdmin.getPhonenumber()
+//        );
+//        adminRepository.save(admin);
+//        return toResponse(admin);
+//    }
+//
+//    //관리자 역할 변경
+//    @Transactional
+//    public AdminResponse changeRole(Long id, String newRole) {
+//        Admin admin = getOrThrow(id);
+//        admin.changeRole(newRole);
+//        adminRepository.save(admin);
+//        return toResponse(admin);
+//    }
+//    //관리자 상태 변경
+//    @Transactional
+//    public SignupResponse signup(SignupRequest request){
+//
+//        // 이메일 중복 확인
+//        if(adminRepository.findByEmail(request.getEmail()).isPresent()){
+//            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+//        }
+//
+//        admin.changePassword(newPassword);
+//        adminRepository.save(admin);
+//        return toResponse(admin);
+//    }
+//    private AdminResponse toResponse(Admin admin){
+//        return new AdminResponse(
+//                admin.getId(),
+//                admin.getName(),
+//                admin.getEmail(),
+//                admin.getPhonenumber(),
+//                admin.getRole(),
+//                admin.getStatus(),
+//                admin.getCreatedAt(),
+//                admin.getApprovedAt(),
+//                admin.getRejectedAt(),
+//                admin.getRejectionReason()
+//        );
+//    }
+//
+//    // 내부 공통 매서드
+//    private Admin getOrThrow(Long id) {
+//        return adminRepository.findById(id)
+//                .orElseThrow(() -> new ResponseStatusException(
+//                        HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다." + id));
+//    }
 }
