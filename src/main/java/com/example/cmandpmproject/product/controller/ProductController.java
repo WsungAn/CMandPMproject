bb40dd02b8d443ddd1d63d42ff69827b4a8a3a26
gunhee530 <gunhee530@naver.com>
2026-07-16 18:18:50 +0900

    상품 R

--- src/main/java/com/example/cmandpmproject/product/controller/ProductController.java
+++ src/main/java/com/example/cmandpmproject/product/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.example.cmandpmproject.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
@@ -13,8 +15,21 @@ public class ProductController {
 
     private final ProductService productService;
 
+    // 상품 등록
     @PostMapping
     public ProductResponse createProduct(@RequestBody ProductCreateRequest request) {
         return productService.createProduct(request);
+    }
+
+    // 상품 목록 조회
+    @GetMapping
+    public List<ProductResponse> getProducts() {
+        return productService.getProducts();
+    }
+
+    // 상품 상세 조회
+    @GetMapping("/{productId}")
+    public ProductResponse getProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
     }
 }
