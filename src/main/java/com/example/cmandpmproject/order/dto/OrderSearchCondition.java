03a7067b32625ced735a232ebbfce89b122914c1
wsungan <ws960221@gmail.com>
2026-07-18 18:22:49 +0900

    일부수정

--- src/main/java/com/example/cmandpmproject/order/dto/OrderSearchCondition.java
+++ src/main/java/com/example/cmandpmproject/order/dto/OrderSearchCondition.java
@@ -4,6 +4,7 @@ import com.example.cmandpmproject.customer.entity.Customer;
 import com.example.cmandpmproject.product.entity.Product;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.query.SortDirection;
 
 @Getter
 @Setter
@@ -12,6 +13,6 @@ public class OrderSearchCondition {
     private Long customerId;
     private int page = 1;
     private int limit = 10;
-    private SortBy sortBy;
-    private SortOrder sortOrder;
+    private SortBy sortBy = SortBy.DATE;
+    private SortOrder sortOrder = SortOrder.DESC;
 }
