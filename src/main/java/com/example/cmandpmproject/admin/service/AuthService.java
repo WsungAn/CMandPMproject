282ba67243f1ec72d8ce8e0e751d6a2086f23695
lowtempice9489 <b7749abc92@gmail.com>
2026-07-19 18:00:15 +0900

    관리자 인증 담당 postman 테스트 후 부족한 부분 보완하여 커밋(2)

--- src/main/java/com/example/cmandpmproject/admin/service/AuthService.java
+++ src/main/java/com/example/cmandpmproject/admin/service/AuthService.java
@@ -4,7 +4,6 @@ import com.example.cmandpmproject.admin.Config.PasswordEncoder;
 import com.example.cmandpmproject.admin.dto.AuthLoginRequest;
 import com.example.cmandpmproject.admin.dto.AuthSession;
 import com.example.cmandpmproject.admin.entity.Admin;
-import com.example.cmandpmproject.admin.entity.AdminStatus;
 import com.example.cmandpmproject.admin.repository.AdminRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
